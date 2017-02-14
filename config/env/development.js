37529a58cd83936c89250e5cd26b1d9ceb1a1af2
Yuichi <gerar.mz11@hotmail.com>
2017-02-14 18:24:55 -0500

    - Diseño de cabecera - Control de anchura de página

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,7 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/dra',
     options: {
       user: '',
       pass: ''
@@ -22,7 +22,7 @@ module.exports = {
     }
   },
   app: {
-    title: defaultEnvConfig.app.title + ' - Development Environment'
+    title: defaultEnvConfig.app.title + ' - Development Environment',
   },
   facebook: {
     clientID: process.env.FACEBOOK_ID || 'APP_ID',
