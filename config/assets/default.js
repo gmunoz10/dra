1416ac456553a4aa47a8f4acfd78356b7b38a8d9
Yuichi <gerar.mz11@hotmail.com>
2017-02-15 19:00:04 -0500

    - Diseño de carousel de noticias y eventos. - Diseño de pie de página con mapa de acceso, webs recomendadas, ayuda y contacto. - Arreglo de posicionamiento de barra superior a estática.

--- config/assets/default.js
+++ config/assets/default.js
@@ -5,9 +5,12 @@ module.exports = {
     lib: {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.css'
+        'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+        'public/lib/fontawesome/css/font-awesome.css',
       ],
       js: [
+        'public/lib/jquery/dist/jquery.js',
+        'public/lib/bootstrap/dist/js/bootstrap.js',
         'public/lib/angular/angular.js',
         'public/lib/angular-resource/angular-resource.js',
         'public/lib/angular-animate/angular-animate.js',
