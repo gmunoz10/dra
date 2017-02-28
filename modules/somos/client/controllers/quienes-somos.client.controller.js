10d7b8bcd8553ab69488a660af2fd8c738c123f1
Yuichi11 <gerar.mz11@hotmail.com>
2017-02-28 14:51:38 -0500

    Eliminación de estructura de angular 1

--- modules/somos/client/controllers/quienes-somos.client.controller.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// Articles controller
-angular.module('somos').controller('SomosController', ['$scope', '$stateParams', '$location', 'Authentication', 'Somos',
-  function ($scope, $stateParams, $location, Authentication, Somos) {
-    $scope.authentication = Authentication;
-  }
-]);
