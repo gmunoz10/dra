f78a122fea3642f38fcbabfd1e0a3f807d06fae9
Yuichi11 <gerar.mz11@hotmail.com>
2017-03-06 16:08:06 -0500

    Página principal

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,5 @@
+declare var $: any
+
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import { FormGroup, FormControl, Validators, FormBuilder }  from '@angular/forms';
@@ -11,15 +13,16 @@ import { DataService } from '../services/data.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
   label = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cave putes quicquam esse verius. Si mala non sunt, iacet omnis ratio Peripateticorum. At cum de plurimis eadem dicit, tum certe de maximis. Illum mallem levares, quo optimum atque humanissimum virum, Cn. Vide, quantum, inquam, fallare, Torquate. Si quae forte-possumus. Duo Reges: constructio interrete. Itaque hic ipse iam pridem est reiectus; Nam de summo mox, ut dixi, videbimus et ad id explicandum disputationem omnem conferemus. Quare conare, quaeso. Deinde disputat, quod cuiusque generis animantium statui deceat extremum. Non quaeritur autem quid naturae tuae consentaneum sit, sed quid disciplinae. Quid ei reliquisti, nisi te, quoquo modo loqueretur, intellegere, quid diceret?";
 
-  constructor(private http: Http,
-              private dataService: DataService,
-              public toast: ToastComponent,
-              private formBuilder: FormBuilder) { 
-  }
+  constructor() { }
 
+  ngOnInit() {
+  	$('.carousel').carousel({
+ 	  interval: 2000 // in milliseconds
+ 	});
+  }
 
 }
